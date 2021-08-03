4252c93c14a22db0f0cebf3d2aafb57ae474c9d4
sgtbett <kiplangatsgt@gmail.com>
2021-08-03 05:39:39 +0300

    validation and engenuity

--- lib/screens/alert/create_alert.dart
+++ lib/screens/alert/create_alert.dart
@@ -1,6 +1,7 @@
 import 'package:crypto_tracker/constants.dart';
 import 'package:crypto_tracker/screens/home/components/currencyLIst.dart';
 import 'package:flutter/material.dart';
+import 'package:select_form_field/select_form_field.dart';
 
 class CreateAlert extends StatefulWidget {
   const CreateAlert({Key? key}) : super(key: key);
@@ -10,45 +11,32 @@ class CreateAlert extends StatefulWidget {
 }
 
 class _CreateAlertState extends State<CreateAlert> {
-  String _chosenValue = 'one time';
+  final List<Map<String, dynamic>> _items = [
+    {
+      'value': 'ONE TIME',
+      'label': '     ONE TIME',
+      'icon': Icon(Icons.repeat_one),
+    },
+    {
+      'value': 'PERISTENT',
+      'label': '    PERISTENT',
+      'icon': Icon(Icons.repeat),
+    },
+  ];
 
   Widget customSelect() {
-    return DropdownButton<String>(
-      elevation: 20,
-      focusColor: Colors.white,
-      value: _chosenValue,
-      //elevation: 5,
-      style: TextStyle(color: Colors.white),
-      iconEnabledColor: Colors.black,
-      items: <String>[
-        'persistent',
-        'one time',
-      ].map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(
-            value,
-            style: TextStyle(
-              color: kTextMediumColor,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        );
-      }).toList(),
+    return SelectFormField(
+      type: SelectFormFieldType.dropdown,
+      dialogCancelBtn: "close",
+      enableInteractiveSelection: true,
 
-      hint: Text(
-        "select frequency",
-        style: TextStyle(
-          color: kTextLightColor,
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      onChanged: (value) {
-        setState(() {
-          _chosenValue = value.toString();
-        });
-      },
+      // or can be dialog
+      initialValue: 'ONE TIME',
+      icon: Icon(Icons.repeat),
+      labelText: 'SELECT FREQUENCY',
+      items: _items,
+      onChanged: (val) => print(val),
+      onSaved: (val) => print(val),
     );
   }
 
@@ -73,136 +61,145 @@ class _CreateAlertState extends State<CreateAlert> {
           ),
         ),
       ),
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: Text(
-                "Enter Amount",
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                left: 80,
-                right: 80,
-                bottom: 20,
-              ),
-              child: Form(
-                child: TextFormField(
-                  maxLength: 16,
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.numberWithOptions(
-                    decimal: true,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+          new TextEditingController().clear();
+        },
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 60,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "Enter Amount",
+                    textAlign: TextAlign.center,
                   ),
-                  decoration: InputDecoration(
-                    hintText: "0.00",
-                    alignLabelWithHint: true,
-                    enabled: true,
-                    labelStyle: TextStyle(
-                      textBaseline: TextBaseline.ideographic,
-                    ),
-                    prefixText: "\$",
-                    floatingLabelBehavior: FloatingLabelBehavior.always,
-                    contentPadding: EdgeInsets.symmetric(
-                      vertical: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: 80,
+                    right: 80,
+                    bottom: 20,
+                  ),
+                  child: Form(
+                    child: TextFormField(
+                      controller: TextEditingController(),
+                      maxLength: 16,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "0.00",
+                        alignLabelWithHint: true,
+                        // enabled: true,
+                        labelStyle: TextStyle(
+                          textBaseline: TextBaseline.ideographic,
+                        ),
+                        prefixText: "\$",
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 20,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                left: 40,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Select Coins",
-                    style: TextStyle(
-                      color: kTextColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    left: 40,
                   ),
-                  SizedBox(
-                    height: 20,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Select Coins",
+                        style: TextStyle(
+                          color: kTextColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      // coins to be here
+                    ],
                   ),
-                  // coins to be here
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 40,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Alert Frequency",
-                    style: TextStyle(
-                      color: kTextColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 50,
-                    child: customSelect(),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.symmetric(
-                horizontal: 40,
-                vertical: 30,
-              ),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.red[300],
-                  enableFeedback: true,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
                   padding: EdgeInsets.symmetric(
-                    vertical: 18,
+                    horizontal: 40,
                   ),
-                  elevation: 20,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Alert Frequency",
+                        style: TextStyle(
+                          color: kTextColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: customSelect(),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
                   ),
                 ),
-                onPressed: () {},
-                autofocus: true,
-                child: Text(
-                  "Create Alert",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40,
+                    vertical: 30,
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red[300],
+                      enableFeedback: true,
+                      padding: EdgeInsets.symmetric(
+                        vertical: 18,
+                      ),
+                      elevation: 20,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {},
+                    autofocus: true,
+                    child: Text(
+                      "Create Alert",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
